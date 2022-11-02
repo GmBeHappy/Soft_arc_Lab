5ec77d5f62f5f4b9ca5b636a94c4be4cbbc58265
Witchayut Gerdchai <gmplk@hotmail.com>
2022-11-02 13:31:15 +0700

    first commit

---
+++ creational/src/Exporter.java
@@ -0,0 +1,5 @@
+import java.util.Set;
+
+public interface Exporter {
+    public String getMetadataString(Set<Book> books);
+}
---
+++ creational/src/XMLBookMetadataExporter.java
@@ -0,0 +1,9 @@
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+public class XMLBookMetadataExporter extends BookMetadataExporter {
+    @Override
+    public Exporter createExporter() throws IOException, ParserConfigurationException {
+        return new XMLBookMetadataExporterConcrete();
+    }
+}
